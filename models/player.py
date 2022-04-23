3fb2f020ed8279abb92e4b1e8f2d92246efae3ce
Andrew Nourry <andrewnourry@gmail.com>
2022-04-23 17:29:20 +0200

    ajout du docstring de la class Player

--- models/player.py
+++ models/player.py
@@ -1,13 +1,25 @@
 class Player:
-    def __init__(self, first_name, last_name, birth_date, sex, rank=int):
+    def __init__(self, first_name, last_name, birth_date, gender, rank: int):
+        """_summary_
+
+        Args:
+            first_name (_type_): prénom du joueur
+            last_name (_type_): nom du joueur
+            birth_date (_type_): date de naissance, format : xx/xx/xx
+            gender (_type_): Sexe du joueur
+            rank (int): Classement du joueur
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.birth_date = birth_date
-        self.sex = sex
+        self.gender = gender
         self.rank = rank
 
-    def __str__(self):
-        return f"{self.first_name} {self.last_name}, {self.sex}, né(e) le {self.birth_date} de rang {self.rank}"
+    def __str__(self) -> str:
+        return (
+            f"{self.first_name} {self.last_name}, {self.sex}, "
+            f"né(e) le {self.birth_date} (rang {self.rank})"
+        )
 
 
 """class PlayerList(list):
