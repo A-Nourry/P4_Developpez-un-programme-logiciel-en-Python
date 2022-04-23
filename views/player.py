91f699c6890d745d64ef75495e2f30fc2ca81825
Andrew Nourry <andrewnourry@gmail.com>
2022-04-23 17:27:43 +0200

    ajout du fichier player.py dans views

---
+++ views/player.py
@@ -0,0 +1,20 @@
+class PlayerView:
+    def prompt_first_name():
+        first_name = input("Prénom du joueur: ")
+        return first_name
+
+    def prompt_last_name():
+        last_name = input("Nom du joueur: ")
+        return last_name
+
+    def prompt_birth_date():
+        birth_date = input("Date de naissance: ")
+        return birth_date
+
+    def prompt_gender():
+        gender = input("homme ou femme ? ")
+        return gender
+
+    def prompt_rank():
+        rank = input("classement ? ")
+        return rank
