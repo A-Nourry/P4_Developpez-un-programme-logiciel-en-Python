d89961578a0ff7a2b34f2c545b1b14da90253da0
Andrew Nourry <andrewnourry@gmail.com>
2022-04-24 23:21:38 +0200

    création de la class Match

---
+++ models/match.py
@@ -0,0 +1,12 @@
+class Match:
+    def __init__(self, player_one, player_two, result):
+        """_summary_
+
+        Args:
+            player_one (_type_):
+            player_two (_type_):
+            result (_type_):
+        """
+        self.player_one = player_one
+        self.player_two = player_two
+        self.result = result
