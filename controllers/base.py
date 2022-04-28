21117833350f87a1e90610dd56ecc20a4848a1fb
Andrew Nourry <andrewnourry@gmail.com>
2022-04-29 00:37:13 +0200

    modification du controlleur
    ajout des 3 fonctions generate_pairs() add_match() et new_round()
    
    MàJ du run()

--- controllers/base.py
+++ controllers/base.py
@@ -1,5 +1,7 @@
 from models.player import Player
 from models.tournament import Tournament
+from models.match import Match
+from models.round import Round
 
 
 class Controller:
@@ -9,6 +11,7 @@ class Controller:
         self.view = view
 
     def add_player(self, tournament: Tournament):
+        """add a new player to the player dict from a Tournament object"""
         if tournament is not None:
             new_player = self.view.prompt_new_player()
 
@@ -20,7 +23,7 @@ class Controller:
                 rank=new_player["rank"],
             )
 
-            tournament.add_player(player)
+            tournament.add_player_to_dict(player)
 
             return True
 
@@ -36,12 +39,55 @@ class Controller:
         )
         return tournament
 
+    def generate_pairs(self, player_dict):
+        """generate pairs of players from a dict"""
+
+        first_pair = (player_dict["Joueur 1"], player_dict["Joueur 2"])
+        second_pair = (player_dict["Joueur 3"], player_dict["Joueur 4"])
+        third_pair = (player_dict["Joueur 5"], player_dict["Joueur 6"])
+        fourth_pair = (player_dict["Joueur 7"], player_dict["Joueur 8"])
+
+        return first_pair, second_pair, third_pair, fourth_pair
+
+    def add_match(self, pair_of_players, match_round: Round):
+        """instantiate Match object and add it to the list of matches from Round object"""
+
+        if match_round is not None:
+            match = Match(pair_of_players[0], pair_of_players[1])
+            match_round.add_match_to_list(match)
+
+            return True
+
+    def new_round(self, round_number):
+        """instantiate Round object and return it"""
+
+        new_round = Round(f"Round + {round_number}", "start_time", "end_time")
+        return new_round
+
     def run(self):
+        # Tournament creation
         print("NOUVEAU TOURNOI")
         tournament = self.new_tournament()
 
-        print("AJOUT DES JOUEURS")
-        for _ in range(tournament.MAX_NUMBER_PLAYER):
+        # Players creation
+        print(f"AJOUT DES {tournament.MAX_NUMBER_PLAYER} JOUEURS")
+        for i in range(tournament.MAX_NUMBER_PLAYER):
+            print("JOUEUR", i + 1)
             self.add_player(tournament)
 
-        print(tournament.players)
+        # Rounds
+        for rounds in tournament.rounds:
+            print(f"TOUR {rounds}")
+            new_round = self.new_round(f"TOUR {rounds}")
+            print(f"génération des paires de joueurs pour le tour {rounds}...")
+            pairs = self.generate_pairs(tournament.players)
+            print(pairs)
+
+            print("génération des matchs...")
+            for pair in pairs:
+                self.add_match(pair, new_round)
+
+            for matches in new_round.list_of_matches:
+                print(matches)
+                match = matches
+                match.play_match()
