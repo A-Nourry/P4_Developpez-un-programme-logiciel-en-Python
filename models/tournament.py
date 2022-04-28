6d973783dafc3ea3096ae512d7e7c52b7759c44f
Andrew Nourry <andrewnourry@gmail.com>
2022-04-29 00:39:40 +0200

    modification du modèle Tournament
    méthode add_player() remplacé par la méthode add_player_dict()

--- models/tournament.py
+++ models/tournament.py
@@ -1,7 +1,8 @@
 class Tournament:
     def __init__(self, name, location, date, time_rule, max_number_player=8, rounds=4):
 
-        self.players = []
+        self.players = {}
+        self.rounds = []
 
         self.name = name
         self.location = location
@@ -10,5 +11,11 @@ class Tournament:
         self.MAX_NUMBER_PLAYER = max_number_player
         self.rounds = rounds
 
-    def add_player(self, player):
-        self.players.append(player)
+    def add_player_to_dict(self, player):
+
+        for i in range(self.MAX_NUMBER_PLAYER):
+
+            self.players[f"Joueur {i + 1}"] = player
+
+    def add_round(self, round):
+        self.rounds.append(round)
