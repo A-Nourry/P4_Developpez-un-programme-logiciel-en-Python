5e2d504a1b2c3dcbd8f3ef441118a421fca304a8
Andrew Nourry <andrewnourry@gmail.com>
2022-04-29 00:37:49 +0200

    modification du modèle Match
    ajout de la méthode play_match()

--- models/match.py
+++ models/match.py
@@ -1,12 +1,18 @@
 class Match:
-    def __init__(self, player_one, player_two, result):
+    def __init__(self, player_one, player_two):
         """_summary_
 
         Args:
-            player_one (_type_):
-            player_two (_type_):
-            result (_type_):
+            player_one (_type_):instance of Player
+            player_two (_type_):instance of Player
         """
         self.player_one = player_one
         self.player_two = player_two
-        self.result = result
+
+    def play_match(self):
+        player_one_result = input(f"Score de {self.player_one}: ")
+        player_two_result = input(f"Score de {self.player_two}: ")
+        return player_one_result, player_two_result
+
+    def __str__(self):
+        return f"{self.player_one} affronte {self.player_two}"
