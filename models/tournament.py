af34171066db7cdd715324c9cf8fb965bca56b5c
Andrew Nourry <andrewnourry@gmail.com>
2022-05-01 21:45:21 +0200

    ajoute d'une methode __repr__()

--- models/tournament.py
+++ models/tournament.py
@@ -1,5 +1,7 @@
 class Tournament:
-    def __init__(self, name, location, date, time_rule, max_number_player=8, rounds=4):
+    def __init__(
+        self, name, location, date, time_rule, max_number_player=8, round_number=4
+    ):
 
         self.players = {}
         self.rounds = []
@@ -9,10 +11,13 @@ class Tournament:
         self.date = date
         self.time_rule = time_rule
         self.MAX_NUMBER_PLAYER = max_number_player
-        self.rounds = rounds
+        self.round_number = round_number
 
     def add_player_to_dict(self, player_number, player):
         self.players[player_number] = player
 
-    def add_round(self, round):
-        self.rounds.append(round)
+    def add_round(self, rounds):
+        self.rounds.append(rounds)
+
+    def __repr__(self):
+        return f"{self.name}"
