a86b5ad188e1c7ff5885bdd20a524c5322db96ab
Andrew Nourry <andrewnourry@gmail.com>
2022-05-05 21:02:15 +0200

    ajout des méthodes start_timestamp() et end_timestamp()

--- models/round.py
+++ models/round.py
@@ -1,18 +1,40 @@
+from datetime import datetime, date
+
+
 class Round:
-    def __init__(self, name, start_times, end_times):
-        """_summary_
-
-        Args:
-            name (_type_): name of the round
-            start_times (_type_): starting round date, format : xx/xx/xx à xx:xx
-            end_times (_type_): ending round date, format : xx/xx/xx à xx:xx
-        """
-
+    def __init__(self, name):
         self.list_of_matches = []
 
         self.name = name
-        self.start_times = start_times
-        self.end_times = end_times
+        self.start_time = ""
+        self.end_time = ""
+
+    def start_timestamp(self):
+        """returns current time"""
+        now = datetime.now()
+        today = date.today()
+
+        current_date = today.strftime("%d/%m/%Y")
+        current_time = now.strftime("%H:%M")
+
+        time = f"{current_date} à {current_time}"
+
+        self.start_time = time
+
+    def end_timestamp(self):
+        """returns current time"""
+        now = datetime.now()
+        today = date.today()
+
+        current_date = today.strftime("%d/%m/%Y")
+        current_time = now.strftime("%H:%M")
+
+        time = f"{current_date} à {current_time}"
+
+        self.end_time = time
 
     def add_match_to_list(self, match):
         self.list_of_matches.append(match)
+
+    def display_round(self):
+        return {"Matchs": self.list_of_matches}
