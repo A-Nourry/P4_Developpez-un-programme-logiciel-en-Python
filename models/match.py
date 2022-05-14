da729dad410b7c822ae4ccd9c9ee5af15842343f
Andrew Nourry <andrewnourry@gmail.com>
2022-05-14 21:57:02 +0200

    Ajout de la méthode save() pour la sérialisation

--- models/match.py
+++ models/match.py
@@ -1,3 +1,9 @@
+from tinydb import TinyDB
+
+
+match_db = TinyDB("saves/" + "match_db.json")
+
+
 class Match:
     def __init__(self, player_one, player_two):
         """_summary_
@@ -9,16 +15,26 @@ class Match:
         self.player_one = player_one
         self.player_two = player_two
 
-        self.player_one_result = None
-        self.player_two_result = None
+        self.player_one_score = 0.0
+        self.player_two_score = 0.0
 
-        self.match = ([self.player_one, self.player_one_result], [self.player_two, self.player_two_result])
+        self.match = ([self.player_one, self.player_one_score], [self.player_two, self.player_two_score])
 
     def display_match_result(self):
         return {
-            self.player_one: self.player_one_result,
-            self.player_two: self.player_two_result
+            "Matchs": f"{self.player_one} contre {self.player_two}",
+            "Résultats": f"{self.player_one_score} - {self.player_two_score}"
         }
+
+    def save(self):
+        serialized_match = {
+            "Joueur 1": self.player_one,
+            "Joueur 2": self.player_two,
+            "Score J1": self.player_one_score,
+            "Score J2": self.player_two_score
+        }
+
+        match_db.insert(serialized_match)
 
     def __str__(self):
         return f"{self.player_one} contre {self.player_two}"
