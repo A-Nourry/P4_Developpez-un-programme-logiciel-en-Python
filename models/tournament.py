d67028221232d39be9f1b0b42a12959ca7f03b74
Andrew Nourry <andrewnourry@gmail.com>
2022-05-15 02:03:22 +0200

    ajout des méthodes display_tournament() et save()

--- models/tournament.py
+++ models/tournament.py
@@ -1,3 +1,9 @@
+from tinydb import TinyDB
+
+
+tournament_db = TinyDB("saves/tournament_db.json")
+
+
 class Tournament:
     def __init__(
         self, name, location, date, time_rule, max_number_player=8, round_number=4
@@ -14,6 +20,31 @@ class Tournament:
         self.MAX_NUMBER_PLAYER = max_number_player
         self.round_number = round_number
 
+    def display_tournament(self):
+        return {
+            "Nom": self.name,
+            "Lieu": self.location,
+            "Date": self.date,
+            "Règle du temps": self.time_rule,
+            "Nombre de joueurs": self.MAX_NUMBER_PLAYER,
+            "Nombre de tours": self.round_number,
+        }
+
+    def save(self):
+        serialized_tournament = {
+            "Nom": self.name,
+            "Lieu": self.location,
+            "Date": self.date,
+            "Règle du temps": self.time_rule,
+            "Nombre de joueurs": self.MAX_NUMBER_PLAYER,
+            "Nombre de tours": self.round_number,
+            "Description": self.description,
+            "Tours": self.rounds,
+            "Joueurs": self.players,
+        }
+
+        tournament_db.insert(serialized_tournament)
+
     def add_player_to_dict(self, player_number, player):
         self.players[player_number] = player
 
@@ -22,3 +53,11 @@ class Tournament:
 
     def __repr__(self):
         return f"{self.name}"
+
+
+def load_tournaments():
+    serialized_tournaments = []
+    for tournaments in tournament_db:
+        serialized_tournaments.append(tournaments)
+
+    return serialized_tournaments
