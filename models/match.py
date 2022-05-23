f97dbf3ca5a91bd09d0f6ea9b64f4f980c4e9e13
Andrew Nourry <andrewnourry@gmail.com>
2022-05-23 16:38:48 +0200

    ajout de la fonction load_matches()
    + modifications mineurs

--- models/match.py
+++ models/match.py
@@ -1,11 +1,12 @@
 from tinydb import TinyDB
 
 
-match_db = TinyDB("saves/" + "match_db.json")
+db = TinyDB("db.json")
+matches_table = db.table("matches")
 
 
 class Match:
-    def __init__(self, player_one, player_two):
+    def __init__(self, player_one, player_two, m_id):
         """_summary_
 
         Args:
@@ -14,27 +15,39 @@ class Match:
         """
         self.player_one = player_one
         self.player_two = player_two
+        self.m_id = m_id
 
         self.player_one_score = 0.0
         self.player_two_score = 0.0
 
-        self.match = ([self.player_one, self.player_one_score], [self.player_two, self.player_two_score])
+        self.match = (
+            [self.player_one, self.player_one_score],
+            [self.player_two, self.player_two_score],
+        )
 
     def display_match_result(self):
         return {
             "Matchs": f"{self.player_one} contre {self.player_two}",
-            "Résultats": f"{self.player_one_score} - {self.player_two_score}"
+            "Résultats": f"{self.player_one_score} - {self.player_two_score}",
         }
 
     def save(self):
         serialized_match = {
-            "Joueur 1": self.player_one,
-            "Joueur 2": self.player_two,
-            "Score J1": self.player_one_score,
-            "Score J2": self.player_two_score
+            "player_one": self.player_one,
+            "player_two": self.player_two,
+            "player_one_score": self.player_one_score,
+            "player_two_score": self.player_two_score,
         }
 
-        match_db.insert(serialized_match)
+        matches_table.insert(serialized_match)
 
     def __str__(self):
         return f"{self.player_one} contre {self.player_two}"
+
+
+def load_matches():
+    serialized_matches = []
+    for matches in matches_table:
+        serialized_matches.append(matches)
+
+    return serialized_matches
