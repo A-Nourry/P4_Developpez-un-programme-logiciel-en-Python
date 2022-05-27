f0f9f2c4d09531432123e616a0b62f925c48f037
Andrew Nourry <andrewnourry@gmail.com>
2022-05-27 08:12:06 +0200

    ajout de docstrings

--- models/match.py
+++ models/match.py
@@ -8,11 +8,11 @@ matches = Query()
 
 class Match:
     def __init__(self, player_one, player_two, m_id):
-        """_summary_
+        """Match instance attributes
 
         Args:
-            player_one (_type_):instance of Player
-            player_two (_type_):instance of Player
+            player_one (instance):instance of Player
+            player_two (instance):instance of Player
         """
         self.player_one = player_one
         self.player_two = player_two
@@ -27,12 +27,16 @@ class Match:
         )
 
     def display_match_result(self):
+        """display match results in a dict
+        """
         return {
             "Matchs": f"{self.player_one} contre {self.player_two}",
             "Résultats": f"{self.player_one_score} - {self.player_two_score}",
         }
 
     def save(self):
+        """save Match attributes in the data base
+        """
         serialized_match = {
             "player_one": self.player_one,
             "player_two": self.player_two,
@@ -44,6 +48,12 @@ class Match:
         matches_table.insert(serialized_match)
 
     def update(self, key, value):
+        """update Match attributes in the data base
+
+        Args:
+            key (str): key of the data base's dict
+            value (_type_): value of the data base's dict
+        """
         matches_table.update({key: value}, matches.m_id == self.m_id)
 
     def __str__(self):
@@ -51,6 +61,11 @@ class Match:
 
 
 def load_matches():
+    """get Match attributes from the data base and return them
+
+    Returns:
+        dict: dict of attributes
+    """
     serialized_matches = []
     for matches in matches_table:
         serialized_matches.append(matches)
